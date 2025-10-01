b9034a62932c8c21c3ff64a66988d52066da7ce3
Arthur Wouters <arthur@grinjo.nl>
2025-10-01 12:10:10 +0200

    Add Location to Event

--- src/DataFixtures/EventFixtures.php
+++ src/DataFixtures/EventFixtures.php
@@ -7,14 +7,22 @@ namespace App\DataFixtures;
 use App\DataFixtures\Trait\Numbers;
 use App\Factory\EventFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class EventFixtures extends Fixture
+class EventFixtures extends Fixture implements DependentFixtureInterface
 {
     use Numbers;
 
     public function load(ObjectManager $manager): void
     {
         EventFactory::createMany(self::EVENTS * self::MULTIPLIER);
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            LocationFixtures::class,
+        ];
     }
 }
--- src/Dto/Event/Response/EventCollectionResponseDto.php
+++ src/Dto/Event/Response/EventCollectionResponseDto.php
@@ -8,9 +8,15 @@ use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use App\Dto\Event;
+use App\Dto\Location\Response\LocationResponseDto;
 use App\Dto\ResponseDto;
 use App\Entity\Event as EventEntity;
+use DateInterval;
+use DateTimeImmutable;
+use Symfony\Component\Serializer\Attribute\Context;
 use Symfony\Component\Serializer\Attribute\SerializedName;
+use Symfony\Component\Serializer\Normalizer\DateIntervalNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -34,5 +40,19 @@ final readonly class EventCollectionResponseDto implements ResponseDto
 
         #[SerializedName('name'), Assert\NotBlank]
         public string $name,
+
+        #[Context(
+            normalizationContext: [DateTimeNormalizer::FORMAT_KEY => 'Y-m-d H:i:s'],
+        )]
+        public DateTimeImmutable $startDatetime,
+
+        #[Context(
+            normalizationContext: [DateTimeNormalizer::FORMAT_KEY => 'Y-m-d H:i:s'],
+        )]
+        public DateTimeImmutable $endDatetime,
+
+        #[SerializedName('location')]
+        #[ApiProperty(readableLink: false)]
+        public ?LocationResponseDto $location = null,
     ) {}
 }
